d9020c46d385211f5280f77b7154b34dc86c275c
Shuhao Zhang <shuhao.zhang@sap.com>
2018-04-04 12:36:19 +0800

    add <who> parameter

--- hello_world_mod.c
+++ hello_world_mod.c
@@ -1,15 +1,22 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+
+#include <linux/moduleparam.h> //for module_param
 MODULE_LICENSE("GPL");
+
+static char *who = "world";
+module_param(who, charp, S_IRUSR|S_IWUSR);
+MODULE_PARM_DESC(who, "Tell me your name.");
+
 static int hello_init(void)
 {
-printk(KERN_ALERT "Hello, world\n");
+printk(KERN_ALERT "Hello, %s!\n", who);
 return 0;
 }
 static void hello_exit(void)
 {
-printk(KERN_ALERT "Goodbye, cruel world\n");
+printk(KERN_ALERT "Goodbye, %s!\n", who);
 }
 module_init(hello_init);
 module_exit(hello_exit);
